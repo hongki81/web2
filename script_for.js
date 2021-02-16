55af06e0907d8f4cae2a26f2cdde450c245fbee9
김홍기 <hongki@gimhong-giui-MacBook-Pro.local>
2021-02-16 09:20:51 +0900

    js Study

---
+++ script_for.js
@@ -0,0 +1,11 @@
+// var i = 0;
+// while(i < 5) {
+//     document.write((i+1) + ': Hello! World<br>');
+//     i = i + 1;
+// }
+
+var total = 10;
+for(var j=0; j < total; j++) {
+    document.write((j+1) + ': Hello! World <br>');
+}
+
